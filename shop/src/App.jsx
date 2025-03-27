b3a7af92587c615b714c82f7eecb5195f6685ca2
Rebootne <rrrricloud02@gmail.com>
2025-03-27 10:52:33 +0500

    Shop

---
+++ shop/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Footer from './footer'
+import Header from './header'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  )
+
+}
+export default App
