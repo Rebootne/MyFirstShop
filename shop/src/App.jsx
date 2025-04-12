82f5fed6ac4196a7691ab119c0735b81eb6fe85e
Rebootne <rrrricloud02@gmail.com>
2025-04-12 14:12:43 +0500

    main-12.04

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -1,14 +1,85 @@
 import { useState } from 'react'
+import React from 'react'
 import Footer from './footer'
 import Header from './header'
+import Items from './items'
+import Item from './item'
 
-function App() {
-  return (
-    <>
-    <Header />
-    <Footer />
-    </>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: [],
+      items: [
+        {
+          id: 1,
+          title: 'Кровать',
+          img: 'bed.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum facilis quaerat architecto pariatur.',
+          category: 'bed', 
+          price: '149.99'
+        },
+        {
+          id: 2,
+          title: 'Стул',
+          img: 'chair.png',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum facilis quaerat architecto pariatur.',
+          category: 'chair', 
+          price: '49.99'
+        },
+        {
+          id: 3,
+          title: 'Прикроватная тумба',
+          img: 'bed_table.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum facilis quaerat architecto pariatur.',
+          category: 'bed_table', 
+          price: '84.99'
+        },
+        {
+          id: 4,
+          title: 'Шкаф',
+          img: 'cupboard.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum facilis quaerat architecto pariatur.',
+          category: 'cupboard', 
+          price: '199.99'
+        },
+        {
+          id: 5,
+          title: 'Зеркало',
+          img: 'mirror.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum facilis quaerat architecto pariatur.',
+          category: 'mirror', 
+          price: '134.99'
+        },
+        {
+          id: 6,
+          title: 'Стол',
+          img: 'table.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum facilis quaerat architecto pariatur.',
+          category: 'table', 
+          price: '399.99'
+        },
+        
+      ]
+    }
 
-}
+    this.addToOrder = this.addToOrder.bind(this)
+  }
+  render () {
+    return (
+      <div className='wrapper'>
+      <Header />
+      <Items items={this.state.items} onAdd={this.addToOrder}/>
+      <Footer />
+      </div>
+    )
+  }
+
+
+  addToOrder(item) {
+    this.setState({ orders: [...this.state.orders, item] }, () => {
+      console.log(this.state.orders)
+    })
+  }
+}  
 export default App
--- shop/src/header.jsx
+++ shop/src/header.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart  } from "react-icons/fa";
 
 export default function header() {
+  let [cartOpen, setCartOpen] = useState(false)
+
   return (
     <header>
         <div>
-            <span className="logo"></span>
+            <span className="logo">House Staff</span>
+            <ul className='nav'>
+              <li>О нас</li>
+              <li>Контакты</li>
+              <li>Кабинет</li>
+            </ul>
+            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/> 
+
+            {cartOpen && (
+                <div className='shop-cart'>
+
+                </div>
+            )}
+
         </div>
         <div className="presentation"></div>
     </header>
